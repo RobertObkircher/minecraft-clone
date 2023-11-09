7c5e409e37a01afef5446df2622ebac516a95fae
Robert Obkircher <robert.obkircher@gmail.com>
2023-11-09 18:45:27 +0100

    add 2d perlin noise

--- src/noise.rs
+++ src/noise.rs
@@ -1,8 +1,10 @@
 use std::num::Wrapping;
+
 use rand::prelude::SliceRandom;
 use rand::rngs::StdRng;
 
 /// Based on the Improved Noise reference implementation by Ken Perlin: https://mrl.cs.nyu.edu/~perlin/noise/
+/// For the 2d version I also looked at https://rtouti.github.io/graphics/perlin-noise-algorithm
 pub struct ImprovedNoise {
     permutation: [u8; 256],
 }
@@ -63,6 +65,34 @@ impl ImprovedNoise {
              ),
         )
     }
+
+    #[allow(non_snake_case)]
+    pub fn noise_2d(&self, mut x: f64, mut y: f64) -> f64 {
+        let X = Wrapping(x.floor() as i32 as u8);
+        let Y = Wrapping(y.floor() as i32 as u8);
+
+        x -= x.floor();
+        y -= y.floor();
+
+        let u = fade(x);
+        let v = fade(y);
+
+        let p = |i: Wrapping<u8>| Wrapping(self.permutation[i.0 as usize]);
+
+        let A = p(X) + Y;
+        let B = p(X + Wrapping(1)) + Y;
+
+        lerp(v,
+             lerp(u,
+                  grad_2(p(A), x, y),
+                  grad_2(p(B), x - 1.0, y),
+             ),
+             lerp(u,
+                  grad_2(p(A + Wrapping(1)), x, y - 1.0),
+                  grad_2(p(B + Wrapping(1)), x - 1.0, y - 1.0),
+             ),
+        )
+    }
 }
 
 fn fade(t: f64) -> f64 {
@@ -79,4 +109,14 @@ fn grad(hash: Wrapping<u8>, x: f64, y: f64, z: f64) -> f64 {
     let u = if h < 8 { x } else { y };
     let v = if h < 4 { y } else if h == 12 || h == 14 { x } else { z };
     return if (h & 1) == 0 { u } else { -u } + if (h & 2) == 0 { v } else { -v };
+}
+
+fn grad_2(hash: Wrapping<u8>, x: f64, y: f64) -> f64 {
+    match hash.0 & 3 {
+        0b00 => 1.0 * x + 1.0 * y,
+        0b01 => -1.0 * x + 1.0 * y,
+        0b10 => 1.0 * x + -1.0 * y,
+        0b11 => -1.0 * x + -1.0 * y,
+        _ => unreachable!()
+    }
 }
